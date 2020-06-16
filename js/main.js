044ad8e5c807f5913dbcc2be48d1cfa28f965376
aleshabaranov <61497343+aleshabaranov@users.noreply.github.com>
2020-06-16 12:09:14 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,3 +5,41 @@ $(function(){
         variableWidth: true
     });
 });
+
+
+$(function(){
+    $('.partners__slider').slick({
+        autoplay: true,
+        arrows: false,
+        variableWidth: true
+    });
+});
+
+new WOW().init();
+
+$(document).ready(function(){
+
+        $("#menu").on("click","a", function (event) {
+            //отменяем стандартную обработку нажатия по ссылке
+            event.preventDefault();
+     
+            //забираем идентификатор бока с атрибута href
+            var id  = $(this).attr('href'),
+     
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+                top = $(id).offset().top;
+             
+            //анимируем переход на расстояние - top за 1500 мс
+            $('body,html').animate({scrollTop: top}, 1500);
+        });
+    });
+
+    $(document).ready(function(){
+        PopUpHide();
+    });
+    function PopUpShow(){
+        $("#modal").show();
+    }
+    function PopUpHide(){
+        $("#modal").hide();
+    }
